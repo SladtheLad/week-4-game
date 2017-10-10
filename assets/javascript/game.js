e7288f8e53ee3c0d6fee527c6e99db0dd30cbcab
nickoliasxii <nickoliasxii@gmail.com>
2017-10-10 14:00:26 -0500

    images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,7 @@ $("#number-to-guess").text(targetNumber);
 var counter = 0;
 var numberOptions = [10, 5, 3, 1];
 
-
+// I still want to figure out how to append different images in this for loop*****************8
 for (var i = 0; i < numberOptions.length; i++) {
     var imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
@@ -19,7 +19,7 @@ $(".crystal-image").on("click", function () {
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
-    
+
     alert("New score: " + counter);
 
     if (counter === targetNumber) {
