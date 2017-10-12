60fe820c3b40abe4b7ecd9b5606ea4506ab87308
nickoliasxii <nickoliasxii@gmail.com>
2017-10-12 16:40:06 -0500

    meh

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,6 +7,8 @@ var images = [ "fizgig.jpg", "Aughra.jpg", "Kira.jpg", "Jen.jpg"];
 var imageCharacter;
 var characterValue;
 
+
+// Still need to append wins or losses to the screen**********
 var wins = 0;
 $("#wins").append(wins);
 
@@ -14,21 +16,22 @@ var losses = 0;
 $("#losses").append(losses);
 
 var counter = 0;
+var currentTotal;
 
-var reset = $("<img>", {id: "reset", src: "assets/images/DarkCrystalPoster.jpg"});
-$("#reset").append(reset);
+
 
 var numberOptions = [Math.floor(Math.random() * ((12 - 1) + 1) + 1), Math.floor(Math.random() * ((12 - 1) + 1) + 1), 
     Math.floor(Math.random() * ((12 - 1) + 1) + 1), Math.floor(Math.random() * ((12 - 1) + 1) + 1),];
 
 
-//Creates a for loop to create my images to click
+//Creates a for loop to create my images to click and assigns them a number
 for (var i = 0; i < numberOptions.length; i++) {
     imageCharacter = $("<img>", {class: "character-image", css: {margin: "0 50px 0 50px"}, src: "assets/images/" + images[i]});
     imageCharacter.addClass("character-image");
     imageCharacter.attr("data-characterValue", numberOptions[i]);
     $("#characters").append(imageCharacter);
 }
+
 
 
 //My click event
@@ -53,12 +56,10 @@ $(".character-image").on("click", function () {
 });
 
 //Reset click event
+var reset = $("<img>", {id: "reset", src: "assets/images/DarkCrystalPoster.jpg"});
+$("#reset").append(reset);
+
 $("#reset").on("click", function() {
     counter = 0;
     characterValue = 0;
-    numberToGuess = Math.floor(Math.random() * ((120 - 19) + 1) + 19);
-    numberOptions = [Math.floor(Math.random() * ((12 - 1) + 1) + 1), Math.floor(Math.random() * ((12 - 1) + 1) + 1),
-    Math.floor(Math.random() * ((12 - 1) + 1) + 1), Math.floor(Math.random() * ((12 - 1) + 1) + 1),];
-    alert("You restarted!");
-    return false;
 });
